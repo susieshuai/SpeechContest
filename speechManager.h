a5cd6af6f0e82b35e73847fa98d2771806a0676b
shuais <shuai.s@northeastern.edu>
2022-08-07 16:42:38 -0700

    add SpeechManager class

---
+++ speechManager.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+/*
+设计演讲管理类
+功能：
+1. 提供菜单界面与用户交互
+2. 对演讲比赛流程进行控制
+3. 与文件的读写交互
+*/
+class SpeechManager
+{
+public:
+    // 构造函数
+    SpeechManager();
+    // 析构函数
+    ~SpeechManager();
+};
---
+++ speechManger.cpp
@@ -0,0 +1,11 @@
+#include "speechManager.h"
+
+// 构造函数
+SpeechManager::SpeechManager()
+{
+}
+
+// 析构函数
+SpeechManager::~SpeechManager()
+{
+}
