3e6f8f8429e45a831ce9e3d7c7698cf36cd2518c
shuais <shuai.s@northeastern.edu>
2022-08-07 17:34:36 -0700

    set up menu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
+#include "speechManager.h"
 
 int main()
 {
+    // 创建管理类对象
+    SpeechManager sm;
+
+    sm.showMenu();
 
     return 0;
 }
--- speechManager.h
+++ speechManager.h
@@ -14,6 +14,10 @@ class SpeechManager
 public:
     // 构造函数
     SpeechManager();
+
+    // 展示菜单
+    void showMenu();
+
     // 析构函数
     ~SpeechManager();
 };
